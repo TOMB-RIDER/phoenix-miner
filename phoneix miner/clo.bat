d29212fab86245bff0f78fe978feb36aa4ed83cd
saikohli <sai.17.2.1999@gmail.com>
2020-05-16 17:53:29 +0530

    Add files via upload

---
+++ phoneix miner/clo.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+PhoenixMiner.exe -pool stratum+tcp://pool.callistopool.org:8008 -wal 0xC534Db5B020e7D490A650eA9ebF6d8Cf9EA5Ce68 -worker rig1 -pass x -proto 2 -coin clo
---
+++ phoneix miner/etc.bat
@@ -0,0 +1,7 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+PhoenixMiner.exe -coin etc -pool etc.2miners.com:1010 -rvram 1 -wal 0x21182cC45BC28B3Ed72f27C3aaEFA65cefb1bB49.RIG-1 -proto 2
+pause
